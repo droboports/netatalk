888a9944f08502d84875ab2161dcc4b9534eaf07
Ricardo Padilha <ricardospadilha@gmail.com>
2016-01-06 21:31:44 -0800

    Updated to netatalk 3.1.8, included 'force user' patch

--- app.sh
+++ app.sh
@@ -1,53 +1,57 @@
+# Don't use these:
 #CFLAGS="${CFLAGS:-} -ffunction-sections -fdata-sections"
 #LDFLAGS="-L${DEST}/lib -L${DEPS}/lib -Wl,--gc-sections"
+# Use only this:
 LDFLAGS="-L${DEST}/lib -L${DEPS}/lib"
 
 ### ZLIB ###
 _build_zlib() {
-# Closest to the one that ships with the 5N (v1.2.3.4)
-local VERSION="1.2.3"
+local VERSION="1.2.8"
 local FOLDER="zlib-${VERSION}"
 local FILE="${FOLDER}.tar.gz"
-local URL="http://downloads.sourceforge.net/project/libpng/zlib/${VERSION}/${FILE}"
+local URL="http://zlib.net/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --prefix="${DEPS}" --shared
+./configure --prefix="${DEPS}"
 make
 make install
+rm -vf "${DEPS}/lib/libz.so"*
 popd
 }
 
 ### OPENSSL ###
 _build_openssl() {
-# Same as the one that ships with the 5N
-local OPENSSL_VERSION="0.9.8n"
-local OPENSSL_FOLDER="openssl-${OPENSSL_VERSION}"
-local OPENSSL_FILE="${OPENSSL_FOLDER}.tar.gz"
-local OPENSSL_URL="http://mirror.switch.ch/ftp/mirror/openssl/source/old/0.9.x/${OPENSSL_FILE}"
+local VERSION="1.0.2e"
+local FOLDER="openssl-${VERSION}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://mirror.switch.ch/ftp/mirror/openssl/source/${FILE}"
 
-_download_tgz "${OPENSSL_FILE}" "${OPENSSL_URL}" "${OPENSSL_FOLDER}"
-pushd target/"${OPENSSL_FOLDER}"
-./Configure --prefix="${DEPS}" shared threads linux-generic32 \
+_download_tgz "${FILE}" "${URL}" "${FOLDER}"
+cp -vf "src/${FOLDER}-parallel-build.patch" "target/${FOLDER}/"
+pushd "target/${FOLDER}"
+patch -p1 -i "${FOLDER}-parallel-build.patch"
+./Configure --prefix="${DEPS}" \
   zlib-dynamic --with-zlib-include="${DEPS}/include" --with-zlib-lib="${DEPS}/lib" \
-  -DL_ENDIAN ${CFLAGS} ${LDFLAGS}
-sed -e "s/-O3//g" -i Makefile
-make -j1
+  no-shared threads linux-armv4 no-ssl2 no-ssl3 -DL_ENDIAN ${CFLAGS} ${LDFLAGS} \
+  -Wa,--noexecstack -Wl,-z,noexecstack
+sed -i -e "s/-O3//g" Makefile
+make
 make install_sw
+rm -vf "${DEPS}/lib/libcrypto.so"* "${DEPS}/lib/libssl.so"*
 popd
 }
 
 ### BDB ###
 _build_bdb() {
-# Same as the one that ships with the 5N
-local VERSION="5.2.42"
+local VERSION="6.1.26"
 local FOLDER="db-${VERSION}"
-local FILE="${FOLDER}.gz"
-local URL="http://download.oracle.com/otn/berkeley-db/${FILE}"
+local FILE="${FOLDER}.tar.gz"
+local URL="http://download.oracle.com/berkeley-db/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}/build_unix"
-../dist/configure --host="${HOST}" --prefix="${DEPS}" --enable-shared --disable-static
+../dist/configure --host="${HOST}" --prefix="${DEPS}" --disable-shared
 make
 make install
 popd
@@ -55,31 +59,30 @@ popd
 
 ### LIBGPG-ERROR ###
 _build_libgpg_error() {
-# Same as the one that ships with the 5N
-local VERSION="1.10"
+local VERSION="1.21"
 local FOLDER="libgpg-error-${VERSION}"
 local FILE="${FOLDER}.tar.bz2"
 local URL="ftp://ftp.gnupg.org/gcrypt/libgpg-error/${FILE}"
 
 _download_bz2 "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --host="${HOST}" --prefix="${DEPS}" --enable-shared --disable-static --enable-threads=posix --disable-doc
-make -j1
+ln -fs lock-obj-pub.arm-unknown-linux-gnueabi.h src/syscfg/lock-obj-pub.linux-gnueabi.h
+./configure --host="${HOST}" --prefix="${DEPS}" --disable-shared --enable-threads=posix --disable-doc
+make
 make install
 popd
 }
 
 ### LIBGCRYPT ###
 _build_libgcrypt() {
-# Same as the one that ships with the 5N
-local VERSION="1.5.0"
+local VERSION="1.6.4"
 local FOLDER="libgcrypt-${VERSION}"
 local FILE="${FOLDER}.tar.bz2"
 local URL="ftp://ftp.gnupg.org/gcrypt/libgcrypt/${FILE}"
 
 _download_bz2 "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --host="${HOST}" --prefix="${DEPS}" --enable-shared --disable-static --with-gpg-error-prefix="${DEPS}"
+./configure --host="${HOST}" --prefix="${DEPS}" --disable-shared --with-gpg-error-prefix="${DEPS}"
 make
 make install
 popd
@@ -94,26 +97,28 @@ local URL="http://download.savannah.gnu.org/releases/attr/${FILE}"
 
 _download_tgz "${FILE}" "${URL}" "${FOLDER}"
 pushd "target/${FOLDER}"
-./configure --host="${HOST}" --prefix="${DEPS}" --enable-shared --disable-static #--enable-static --disable-shared
+./configure --host="${HOST}" --prefix="${DEPS}" --disable-shared
 make
 make install install-dev install-lib
-chmod -v a+x "${DEPS}/lib/libattr.so.1.1.0"
 popd
 }
 
 ### NETATALK ###
 _build_netatalk() {
-local VERSION="3.1.7"
+local VERSION="3.1.8"
 local FOLDER="netatalk-${VERSION}"
 local FILE="${FOLDER}.tar.bz2"
 local URL="http://sourceforge.net/projects/netatalk/files/netatalk/${VERSION}/${FILE}"
 
 _download_bz2 "${FILE}" "${URL}" "${FOLDER}"
-cp -vfa "src/${FOLDER}-increase-flush-interval.patch" "target/${FOLDER}/"
+cp -vf "src/${FOLDER}-force-user.patch" "target/${FOLDER}/"
+cp -vf "src/${FOLDER}-increase-flush-interval.patch" "target/${FOLDER}/"
 pushd "target/${FOLDER}"
+patch -p1 -i "${FOLDER}-force-user.patch"
 patch -p1 -i "${FOLDER}-increase-flush-interval.patch"
 # See: http://sourceforge.net/p/netatalk/bugs/574/
 sed -i '64i#define O_IGNORE 0' "include/atalk/acl.h"
+autoreconf -i
 ./configure --host="${HOST}" --prefix="" \
   --localstatedir="/mnt/DroboFS/System" \
   --with-uams-path="/lib/netatalk" \
@@ -121,11 +126,36 @@ sed -i '64i#define O_IGNORE 0' "include/atalk/acl.h"
   --with-shadow --without-pam \
   --with-cnid-cdb-backend --with-cnid-default-backend=dbd \
   --with-libgcrypt-dir="${DEPS}" --with-bdb="${DEPS}" \
-  --with-ssl-dir="${DEPS}"
+  --with-ssl-dir="${DEPS}" \
+  LIBS="-ldl"
 make
 make install DESTDIR="${DEST}"
-"${STRIP}" -s -R .comment -R .note -R .note.ABI-tag "${DEST}/sbin/"* "${DEST}/bin/"* || true
+"${STRIP}" -s -R .comment -R .note -R .note.ABI-tag \
+  "${DEST}/sbin/"* \
+  "${DEST}/bin/"* \
+  "${DEST}/lib/"*.so \
+  "${DEST}/lib/netatalk/"*.so || true
 popd
+}
+
+_build_rootfs() {
+# bin/attr
+# bin/getfattr
+# bin/hmac256
+# bin/setfattr
+# lib/libatalk.so
+# lib/libatalk.so.17
+# lib/libatalk.so.17.0.0
+# lib/netatalk/uams_dhx2_passwd.so
+# lib/netatalk/uams_dhx_passwd.so
+# lib/netatalk/uams_guest.so
+# lib/netatalk/uams_passwd.so
+# lib/netatalk/uams_randnum.so
+# sbin/afpd
+# sbin/cnid_dbd
+# sbin/cnid_metad
+# sbin/netatalk
+  return 0
 }
 
 _build() {
@@ -134,6 +164,7 @@ _build() {
   _build_bdb
   _build_libgpg_error
   _build_libgcrypt
+  _build_libattr
   _build_netatalk
   _package
 }
